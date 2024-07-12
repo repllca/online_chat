6b4c786702fc81d5c1d0e0d06b6c3bb8261ea4b8
repllca <maridann1022@icloud.com>
2024-07-12 17:46:16 +0900

    first commit

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-
+const port = 3001
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -13,6 +13,6 @@ io.on('connection', (socket) => {
   console.log('a user connected');
 });
 
-server.listen(3001, () => {
-  console.log('listening on *:3001');
+server.listen(port, () => {
+  console.log('success!!➡'+ port);
 });
