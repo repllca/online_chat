7c2f86a7ec372d8e88454ecdfe638e457ac6ca7a
repllca <maridann1022@icloud.com>
2024-07-14 20:28:59 +0900

    change all file

--- app/index.js
+++ app/index.js
@@ -4,15 +4,29 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const port = 3001
+const port = 3001;
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/home', (req, res) => {
+  res.sendFile(__dirname + '/home.html');
+});
+
 io.on('connection', (socket) => {
   console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg); // 受け取ったメッセージをすべてのクライアントに送信
+  });
 });
 
 server.listen(port, () => {
-  console.log('success!!➡'+ port);
+  console.log('success!!➡' + port);
 });
